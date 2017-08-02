a51f6e08922a6d3aa78753dcfd8182be401e0770
saarsta <saarsta@gmail.com>
2017-08-02 10:58:50 +0300

    change id to film_id

--- models/film.js
+++ models/film.js
@@ -6,9 +6,8 @@ var Film = {
         return db.query("Select * from film", callback);
     },
     getFilmById: function (id, callback) {
-        return db.query("select * from film where Id=?", [id], callback);
+        return db.query("select * from film where film_id=?", [id], callback);
     },
-
     getFilmsByAttrs: function (attrs, callback) {
         var offset = attrs.page ? (attrs.page - 1) * MAX_ROWS : 0;
         var query = "select * from film where ";
@@ -23,7 +22,6 @@ var Film = {
         }
 
         if (attrs.category) {
-            /*whereClauseParts.push("film_id in (select film_id from film_category where category_id in (select category_id from category where name like \'%" + attrs.category + "%\'))");*/
             whereClauseParts.push("film_id in (select film_id from film_category where category_id=" + attrs.category + ")");
         }
 
@@ -37,14 +35,14 @@ var Film = {
 
         query += whereClauseParts.join(' and ');
 
-        var fullquery = "select SQL_CALC_FOUND_ROWS A.title as title , A.description as description, A.rating as rating , A.length as length , A.release_year as release_year, lang.name as language, category.name as category, GROUP_CONCAT( distinct CONCAT(actor.first_name,' ', actor.last_name) SEPARATOR ', ')  as actors from " +
-            "( " + query + ") A " +
-            "join language lang on lang.language_id = A.language_id " +
-            "join film_category on film_category.film_id = A.film_id " +
+        var fullquery = "select SQL_CALC_FOUND_ROWS Filtered.title as title , Filtered.description as description, Filtered.rating as rating , Filtered.length as length , Filtered.release_year as release_year, lang.name as language, category.name as category, GROUP_CONCAT( distinct CONCAT(actor.first_name,' ', actor.last_name) SEPARATOR ', ')  as actors from " +
+            "( " + query + ") Filtered " +
+            "join language lang on lang.language_id = Filtered.language_id " +
+            "join film_category on film_category.film_id = Filtered.film_id " +
             "join category on category.category_id = film_category.category_id " +
-            "join film_actor on film_actor.film_id = A.film_id " +
+            "join film_actor on film_actor.film_id = Filtered.film_id " +
             "join actor on actor.actor_id = film_actor.actor_id " +
-            "group by A.title, A.description, A.rating, A.length, A.release_year, lang.name , category.name limit " + offset + "," + MAX_ROWS;
+            "group by Filtered.title, Filtered.description, Filtered.rating, Filtered.length, Filtered.release_year, lang.name , category.name limit " + offset + "," + MAX_ROWS;
 
         console.log("fullquery ===> ", fullquery);
 
